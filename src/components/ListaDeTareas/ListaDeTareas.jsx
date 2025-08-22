f3f9f18e4cd53721cf6987cb186bf4a78c90671b
franco paez <francopaez_11@hotmail.com>
2025-08-21 22:21:43 -0300

    creacion de manejo de lista de tareas y agregar funcionalidad de agregar tareas

--- src/components/ListaDeTareas/ListaDeTareas.jsx
+++ src/components/ListaDeTareas/ListaDeTareas.jsx
@@ -1,7 +1,36 @@
+import { useState } from "react"
+
 const ListaDeTareas=()=>{
+
+    const [input,setInput]=useState("")
+    const [tareas,setTareas]=useState([])
+
+
+    function manejarInput(e){
+        setInput(e.target.value)
+
+    }
+
+    function agregarTarea(){
+        if(input != ""){
+            setTareas([...tareas,input])
+        }
+        else(alert("debes ingresar una tarea"))
+        setInput("")
+
+    }
+
     return(
         <div>
-            <h1>lista de tareas</h1>
+            <h1  >lista de tareas</h1>
+
+            <input type="text" value={input} onChange={manejarInput} />
+            <button onClick={agregarTarea}>agregar tarea</button>
+            <ul>
+                {tareas && tareas.map((tarea,id)=>{
+               return      <li key={id}>{tarea}</li>
+                })}
+            </ul>
         </div>
     )
 }
